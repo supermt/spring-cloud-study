ae6d9c9ceaebe729ccbbb4fed34a9d74e78cedf0
superMT <18007092757@163.com>
2017-05-22 09:42:57 +0800

    Add LifeCycleAction modeling, this project is now abandoned , meet in project Spring Cloud Example

--- code/microservice-infrastructure/src/main/java/edu/uestc/msstudio/cloud/recording/LifeCycleActions.java
+++ code/microservice-infrastructure/src/main/java/edu/uestc/msstudio/cloud/recording/LifeCycleActions.java
@@ -1,27 +1,27 @@
-/**   
+/**
  * Copyright © 2017 微软创新工作室. All rights reserved.
- * 
- * @Title: LifeCycleActions.java 
+ *
+ * @Title: LifeCycleActions.java
  * @Prject: microservice-infrastructure
- * @Package: edu.uestc.msstudio.cloud.recording 
+ * @Package: edu.uestc.msstudio.cloud.recording
  * @Description: TODO
- * @author: MT   
- * @date: 2017年3月13日 下午4:36:47 
- * @version: V1.0   
+ * @author: MT
+ * @date: 2017年3月13日 下午4:36:47
+ * @version: V1.0
  */
 package edu.uestc.msstudio.cloud.recording;
 
-/** 
- * @ClassName: LifeCycleActions 
- * @Description: 
+/**
+ * @ClassName: LifeCycleActions
+ * @Description:
  * this is the life cycle modeling actions
  * @author: MT
- * @date: 2017年3月13日 下午4:36:47  
+ * @date: 2017年3月13日 下午4:36:47
  */
 public enum  LifeCycleActions {
 	createUser,
 	transferUser,
-	mergeUser, 
+	mergeUser,
 	splitUser,
 	queryByUser,
 	queryUserById,
